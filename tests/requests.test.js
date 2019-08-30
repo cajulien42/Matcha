1bd8068463854a0cda42cebe6ecd7296484b089f
Camille JULIEN <cajulien@e1r4p17.42.fr>
2019-08-30 12:39:17 +0200

    testing api in progress

--- tests/requests.test.js
+++ tests/requests.test.js
@@ -1,10 +1,31 @@
 const debug = require('debug')('app:test');
 const each = require('jest-each').default;
-const req = require('./requests');
+// const getUsers = require('./requests');
+const Request = require('./requestsClass');
 
-test('GET request : /api/users, expect list of users', () => (
-  req.then((data) => {
-    expect(data).toBeTruthy();
-  })
-    .catch(err => debug(err))
-));
+// test('GET request : /api/users, expect list of users', () => (
+//   getUsers.then((data) => {
+//     expect(data).toBeTruthy();
+//   })
+//     .catch(err => debug(err))
+// ));
+
+const validUserAuth = {
+  username: 'Jean',
+  password: 'Test123*',
+};
+
+test('GET request : /api/users/user, expect user list, true', async () => {
+  const data = await new Request('/api/users', null).get().catch(err => debug(err));
+  return expect(data).toBeTruthy();
+});
+
+test('GET request : /api/users/user, invalid Auth expect false', async () => {
+  const data = await new Request('/api/users/Claude', null).get().catch(err => debug(err));
+  return expect(data).toBe(false);
+});
+
+test('POST request : /api/auth, expect valid jwt', async () => {
+  const data = await new Request('/api/auth', validUserAuth).authenticate().catch(err => debug(err));
+  return expect(data).toMatch(/^[A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.?[A-Za-z0-9-_.+/=]*$/);
+});
---
+++ tests/requestsClass.js
@@ -0,0 +1,39 @@
+const debug = require('debug')('app:test');
+const axios = require('axios');
+
+module.exports = class Request {
+
+  constructor(route, auth) {
+    this.route = route;
+    this.auth = auth;
+  }
+
+  authenticate() {
+    return (
+      axios.post(`http://localhost:3000${this.route}`, this.auth)
+        .then((response) => {
+          debug('Success: ', response.data.payload);
+          return (response.data.payload);
+        })
+        .catch((error) => {
+          debug('Failure:', error.message);
+          return (false);
+        })
+    );
+  }
+
+  get() {
+    return (
+      axios.get(`http://localhost:3000${this.route}`, { headers: { Authorization: `Bearer ${this.auth}` } })
+        .then((response) => {
+          debug('Success: ', response.data.payload.users);
+          return (response.data.success);
+        })
+        .catch((error) => {
+          debug('Failure:', error.message);
+          return (false);
+        })
+    );
+  }
+
+};
